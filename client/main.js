940ccf1773e3d7ba5066bd843e15957081b54c6d
POSTEL <richard.postel@ynov.com>
2018-02-08 10:26:49 +0100

    [DEV] V1

--- client/main.js
+++ client/main.js
@@ -1,7 +1,15 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
+
 
 import './main.html';
+hosts = new Mongo.Collection('hosts');
+
+Meteor.startup(function () {
+   // Meteor.subscribe('hosts');
+});
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -17,6 +25,117 @@ Template.hello.helpers({
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    //instance.counter.set(instance.counter.get() + 1);
+
+    Meteor.call("testMe", function(error, results) {
+        console.log(results.data.original_title); //results.data should be a JSON object
+        document.getElementById("image").src="http://image.tmdb.org/t/p/w185"+results.data.poster_path;
+        document.getElementById("image").style.visibility='visible';
+    });
   },
 });
+
+Template.personsList.helpers({
+	persons: function(){
+		return Persons.find();
+	}
+});
+
+Template.addPerson.events({
+	'submit':function(event){
+		const modif = event.target.modif.value
+		const pseudo = event.target.pseudo.value;
+		const age = event.target.age.value;
+		if(modif != ""){
+			Persons.update( {"_id": modif}, {pseudo:pseudo, age:age, visible:true});
+			console.log("réussi");
+		}
+		else{
+			Persons.insert({pseudo:pseudo, age:age, visible:true});
+		}	
+		event.target.pseudo.value = '';
+		event.target.age.value = '';
+		event.target.modif.value = '';
+		console.log("Ajout Pseudo:" + pseudo +", Age:" + age);
+		event.preventDefault();
+	}, 
+
+	'click button':function(event){
+		console.log('autre ajout personne');
+	}, 
+	'click .addPerson':function(event){
+		console.log('autre ajout personne par class');
+	}, 
+	'click #addPerson':function(event){
+		console.log('autre ajout personne par id');
+	}, 
+});
+
+/*
+Template.activePerson.helpers({
+	activePerson : function(){
+		return Session.get('activePerson');
+	}
+});*/
+
+Template.personsList.events({
+	'click tr':function(event){
+		//console.log( event.target.innerText);
+		//Session.set('activePerson', event.target.innerText);
+		console.log(event.currentTarget.children[3].lastElementChild.id);
+
+		//console.log(event.currentTarget.children[1].innerText);
+
+		//console.log(event.target);
+
+		document.getElementById("pseudo").value = event.currentTarget.children[0].innerText;
+		document.getElementById("age").value = event.currentTarget.children[1].innerText;
+		document.getElementById("modif").value = event.currentTarget.children[3].lastElementChild.id
+	}, 
+	'click button':function(event){
+		console.log(event.target.id);
+		Persons.remove( {"_id": event.target.id});
+	},
+});
+
+Template.SessionServers.helpers({
+	SessionServers : function(){
+		//console.log( Meteor.status().status);
+		return Meteor.status().status;
+	} 
+});
+
+Template.registerHelper('compare', function(v1, v2) {
+  if (typeof v1 === 'object' && typeof v2 === 'object') {
+    return _.isEqual(v1, v2); // do a object comparison
+  } else {
+    return v1 === v2;
+  }
+});
+
+if (Meteor.isClient) {
+
+   var counter = 0;
+
+   var myInterval = Meteor.setInterval(function() {
+      counter ++
+      console.log("Interval called " + counter + " times...");
+
+
+     Meteor.call("testMe", function(error, results) {
+        console.log(results.data.original_title); //results.data should be a JSON object
+        document.getElementById("image").src="http://image.tmdb.org/t/p/w185"+results.data.poster_path;
+        document.getElementById("image").style.visibility='visible';
+    });
+
+     
+   }, 2000);
+
+   Template.myTemplate.events({
+
+      'click button': function() {
+         Meteor.clearInterval(myInterval);
+         console.log('Interval cleared...')
+      }
+   });
+};
--- server/main.js
+++ server/main.js
@@ -3,3 +3,35 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+Meteor.methods({
+	'testMe'(){
+		console.log('TEST ME!!');
+        //return Meteor.http.call("GET", "http://api.themoviedb.org/3/movie/550?api_key=0325603ffb740cf6e64ffdbe9839255c");
+        var ramdom = Math.floor(Math.random() * 1000) + 1;
+        return Meteor.http.call("GET", "http://api.themoviedb.org/3/movie/"+ ramdom +"?api_key=0325603ffb740cf6e64ffdbe9839255c");
+
+	}
+});
+
+const POLL_INTERVAL = 3000;
+
+Meteor.publish('hosts', function() {
+  console.log('registering hosts publication');
+  const poll = () => {
+    var ramdom = Math.floor(Math.random() * 1000) + 1;
+    console.log(ramdom);
+  	return results = "test"//Meteor.http.call("GET", "http://api.themoviedb.org/3/movie/"+ ramdom +"?api_key=0325603ffb740cf6e64ffdbe9839255c");
+	//document.getElementById("image").src="http://image.tmdb.org/t/p/w185"+results.data.poster_path;
+    //document.getElementById("image").style.visibility='visible';   
+  };
+  poll();
+  this.ready();
+  const interval = Meteor.setInterval(poll, POLL_INTERVAL);
+  this.onStop(function() {
+    	Meteor.clearInterval(interval);
+  });
+});
+
+
